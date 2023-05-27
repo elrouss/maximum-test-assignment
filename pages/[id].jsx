12c93a71f6de9da2dcea22fc3c922cbdecded6a7
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-05-27 17:45:15 +0300

    feat: update eslintrc rules

--- pages/[id].jsx
+++ pages/[id].jsx
@@ -4,7 +4,6 @@ import { URLS } from '../utils/constants';
 
 export const getServerSideProps = async (context) => {
   const { id } = context.params;
-  console.log(id);
 
   const response = await fetch(`${URLS.SHOWCASE}Chery`);
   const data = await response.json();
